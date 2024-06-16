8029b56b001f3bad63891cfb7d4062e18eb96b33
mohits-git <mohitkhatri2805@gmail.com>
2024-06-16 10:11:19 +0530

    feat: add git init command

--- app/main.ts
+++ app/main.ts
@@ -9,15 +9,13 @@ enum Commands {
 
 switch (command) {
     case Commands.Init:
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         console.log("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-        // fs.mkdirSync(".git", { recursive: true });
-        // fs.mkdirSync(".git/objects", { recursive: true });
-        // fs.mkdirSync(".git/refs", { recursive: true });
-        // fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
-        // console.log("Initialized git directory");
+        fs.mkdirSync(".git", { recursive: true });
+        fs.mkdirSync(".git/objects", { recursive: true });
+        fs.mkdirSync(".git/refs", { recursive: true });
+        fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
+        console.log("Initialized git directory");
         break;
     default:
         throw new Error(`Unknown command ${command}`);
