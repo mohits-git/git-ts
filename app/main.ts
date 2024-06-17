8de27baab4d5b762d198803ead779c703bef89bb
mohits-git <mohitkhatri2805@gmail.com>
2024-06-17 08:37:48 +0530

    feat: add 'cat-file -p' command support to read blob content

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,12 @@
-import * as fs from 'fs';
+import * as fs from "fs";
+import zlib from 'node:zlib';
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
     Init = "init",
+    CatFile = "cat-file"
 }
 
 switch (command) {
@@ -16,6 +18,17 @@ switch (command) {
         fs.mkdirSync(".git/refs", { recursive: true });
         fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
         console.log("Initialized git directory");
+        break;
+    case Commands.CatFile:
+        if(args[1] === "-p") {
+            const blobHash = args[2];
+            const filePath = `.git/objects/${blobHash.substring(0, 2)}/${blobHash.substring(2)}`;
+            const data = fs.readFileSync(filePath);
+            const decompressed = zlib.unzipSync(data);
+            const stringData = decompressed.toString('utf8');
+            const content = stringData.split('\0')[1];
+            process.stdout.write(content);
+        }
         break;
     default:
         throw new Error(`Unknown command ${command}`);
