7ef6077bd0192d52d67f7a893c8b734a03c2f0be
mohits-git <mohitkhatri2805@gmail.com>
2024-06-21 10:13:18 +0530

    implement git ls-tree command (wrong implementation, will refactor in next commits)

--- app/main.ts
+++ app/main.ts
@@ -12,6 +12,22 @@ const getShaContent = (sha: string) => {
     return decompressed.toString('utf8');
 }
 
+const getShaFromNext = (entries: string[], i: number): string => {
+    const nextEntry = entries[i + 1].split(' ');
+    let sha;
+    if (nextEntry.length > 1) {
+        const mode = nextEntry[0].substring(nextEntry[0].length - 6);
+        if (mode.endsWith('40000')) sha = nextEntry[0].substring(0, nextEntry[0].length - 5);
+        else sha = nextEntry[0].substring(0, nextEntry[0].length - 6);
+    }
+    else {
+        sha = i + 2 < entries.length
+            ? nextEntry[0] + '\0' + getShaFromNext(entries, i + 1)
+            : nextEntry[0];
+    }
+    return sha;
+}
+
 enum Commands {
     Init = "init",
     CatFile = "cat-file",
@@ -52,20 +68,49 @@ switch (command) {
         }
         break;
     case Commands.LsTree:
+        const sha = args[args.length - 1];
+        const treeObject = getShaContent(sha);
+        console.log(treeObject);
+        console.log('---------------------------')
+        const output: string[] = [];
+        const entries = treeObject.split('\0');
+        console.log(entries);
+        console.log('---------------------------')
         if (args[1] === "--name-only") {
-            const sha = args[2];
-            const treeObject = getShaContent(sha);
-            const output: string[] = [];
-            const entries = treeObject.split('\0');
-            for(let i = 1; i<entries.length; i++) {
+            for (let i = 1; i < entries.length; i++) {
                 const splitEntries = entries[i].split(' ');
-                if(splitEntries.length > 1)
+                if (splitEntries.length > 1)
                     output.push(splitEntries[splitEntries.length - 1]);
             }
-            const outputString = output.join('\n');
-            console.log(outputString);
         }
+        else {
+            for (let i = 1; i < entries.length; i++) {
+                const line = [];
+                const splitEntries = entries[i].split(' ');
+                if (splitEntries.length > 1) {
+                    const mode = splitEntries[0].substring(splitEntries[0].length - 6);
+                    let modeLength = 6;
+                    if (mode.endsWith("40000")) {
+                        line.push("40000");
+                        line.push("tree");
+                        modeLength = 5;
+                    }
+                    else {
+                        line.push(mode);
+                        line.push("blob");
+                    }
+                    const sha = getShaFromNext(entries, i);
+                    const buf = Buffer.from(sha, 'latin1');
+                    const hex = buf.toString('hex');
+                    line.push(hex);
+                    output.push(`${line.join(' ')}\t${splitEntries[splitEntries.length - 1]}`);
+                }
+            }
+        }
+        const outputString = output.join('\n');
+        console.log(outputString);
         break;
     default:
         throw new Error(`Unknown command ${command}`);
 }
+
