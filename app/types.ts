fb50a9c0409e22445e6e523b9e38bb759f848896
mohits-git <mohitkhatri2805@gmail.com>
2024-06-22 20:42:12 +0530

    feat(types): add Modes and Objects Types

--- app/types.ts
+++ app/types.ts
@@ -5,3 +5,15 @@ export enum Commands {
   LsTree = "ls-tree"
 }
 
+export enum Modes {
+  RegularFile = "100644",
+  ExecutableFile = "100755",
+  SymbolicFile = "120000", 
+  Directory = "40000"
+}
+
+export enum Objects {
+  Blob = "blob",
+  Tree = "tree",
+  Commit = "commit"
+}
