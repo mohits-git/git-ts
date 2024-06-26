e15636e618e0e718d61802f93f2d400d5e26f3dd
mohits-git <mohitkhatri2805@gmail.com>
2024-06-26 13:35:53 +0530

    fix: fix hashObject implementation to write files in correct Binary format. UTF8 conversion of fileContent and length calculation currupts the Binary data. Hence, instead zlib compress the Buffer data directly

--- app/commands/hash-object.ts
+++ app/commands/hash-object.ts
@@ -4,12 +4,19 @@ import zlib from "zlib";
 
 export const hashObject = (filePath: string): string | undefined => {
   const fileContent = fs.readFileSync(filePath);
-  const blobContent = `blob ${fileContent.toString('utf8').length}\0${fileContent.toString('utf8')}`
+  const blobContent = Buffer.concat([
+    Buffer.from(`blob ${fileContent.length}\0`),
+    fileContent
+  ]);
   const compressedContent = zlib.deflateSync(blobContent);
-  const hash = crypto.createHash('sha1', compressedContent).digest('hex');
+  const hash = crypto.createHash('sha1').update(compressedContent).digest('hex');
   const dirName = hash.substring(0, 2);
   const blobObjectFile = hash.substring(2);
-  fs.mkdirSync(`./.git/objects/${dirName}`, { recursive: true });
+  try {
+    fs.mkdirSync(`./.git/objects/${dirName}`, { recursive: true });
+  } catch(error) {
+    // Directory already exists, no action needed
+  }
   fs.writeFileSync(`./.git/objects/${dirName}/${blobObjectFile}`, compressedContent);
   return hash;
 }
